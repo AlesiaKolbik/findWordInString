25c8843b50611de07c83d06c1978f25dc283fa57
Alesia Kolbik <moyaalesia@mail.ru>
2017-12-14 16:57:13 +0300

    refactor the function

--- src/com/testpackage/Main.java
+++ src/com/testpackage/Main.java
@@ -13,48 +13,28 @@ public class Main {
         System.out.println("Введите слово:");
         String specifiedWord = scanner1.nextLine();
         ArrayList<String> stringArrayList = split(specifiedString);
-        String lastElementWord = String.valueOf(specifiedWord.charAt(specifiedWord.length()-1));
-        int noMatches = 0;
-        int lengthArray = stringArrayList.size();
-        for (int i = 0; i < lengthArray; i++) {
-            stringArrayList.get(i).endsWith(lastElementWord);
-            if (stringArrayList.get(i).endsWith(lastElementWord)) {
-                System.out.println(stringArrayList.get(i));
-            } else {
-                noMatches++;
+        String lastElementWord = String.valueOf(specifiedWord.charAt(specifiedWord.length() - 1));
+        ArrayList<String> matches = findMatchesWithWord(stringArrayList, lastElementWord);
+        printMatches(matches);
+    }
+
+        private static ArrayList<String> split (String string){
+            String separator = " ";
+            ArrayList<String> stringArray = new ArrayList<>();
+            for (int i = 0; i < string.length(); i++) {
+                int startInd = findNextIndexWord(string, i, separator);
+                int endInd = findLastIndexWord(string, startInd, separator);
+                String word = cutWord(string, startInd, endInd);
+                stringArray.add(word);
+                i = endInd;
             }
+            return stringArray;
         }
-        if (noMatches == lengthArray) {
-            System.out.println("Совпадений на " + lastElementWord + " не найдено.");
-        }
-    }
-    private static ArrayList<String> split(String string) {
-        String separator=" ";
-        ArrayList<String> stringArray=new ArrayList<>();
-        for(int i=0; i<string.length();i++){
-            int startInd=findNextIndexWord(string,i,separator);
-            int endInd=findLastIndexWord(string,startInd , separator);
-            String word=cutWord(string,startInd,endInd);
-            stringArray.add(word);
-            i=endInd;
-        }
-        return stringArray;
-    }
-
-
-
-    private static String cutWord(String string, int startInd, int endInd) {
-        String word = "";
-        for (int i = startInd; i < endInd; i++) {
-            word += string.charAt(i);
-        }
-        return word;
-    }
 
     private static int findLastIndexWord(String string, int index, String separator) {
         while (!separator.contains(String.valueOf(string.charAt(index)))) {
             index++;
-            if(index==string.length()-1)
+            if (index == string.length() - 1)
                 break;
         }
         return index;
@@ -67,4 +47,31 @@ public class Main {
         return index;
     }
 
+    private static String cutWord(String string, int startInd, int endInd) {
+        String word = "";
+        for (int i = startInd; i < endInd; i++) {
+            word += string.charAt(i);
+        }
+        return word;
+    }
+
+    private static ArrayList<String> findMatchesWithWord(ArrayList<String> stringArrayList, String symbol) {
+        ArrayList<String> matches = new ArrayList<>();
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            if (stringArrayList.get(i).endsWith(symbol)) {
+                matches.add(stringArrayList.get(i));
+            }
+        }
+        return matches;
+    }
+
+    private static void printMatches(ArrayList<String> matches) {
+        if (matches.size() == 0) {
+            System.out.println("Совпадений не найдено.");
+        } else {
+            for (int i = 0; i < matches.size(); i++) {
+                System.out.println(matches.get(i));
+            }
+        }
+    }
 }
